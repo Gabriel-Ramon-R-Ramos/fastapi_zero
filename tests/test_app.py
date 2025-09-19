80834dad6eea69d7f46e563b25cbb7c87340d8a4
Gabriel Ramon <garamon97@gmail.com>
2025-09-18 23:30:32 -0300

    refactor: substituindo os endpoint para atender banco de dados real

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,7 @@
 from http import HTTPStatus
 
+from fastapi_zero.schemas import UserPublic
+
 
 def test_read_root(client):
     response = client.get('/')
@@ -30,18 +32,18 @@ def test_read_users(client):
     response = client.get('/users/')
 
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {
-        'users': [
-            {
-                'id': 1,
-                'username': 'alice',
-                'email': 'alice@example.com',
-            }
-        ]
-    }
+    assert response.json() == {'users': []}
 
 
-def test_read_only_users(client):
+def test_read_users_with_user(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get('/users/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'users': [user_schema]}
+
+
+def test_read_only_users(client, user):
     response = client.get('/users/1')
     response_error = client.get('/users/-1')
 
@@ -56,21 +58,21 @@ def test_read_only_users(client):
     assert response_error.json() == {'detail': 'User not found!'}
 
 
-def test_update_user(client):
+def test_update_user(client, user):
     response = client.put(
-        'users/1',
+        '/users/1',
         json={
-            'username': 'bob',
-            'email': 'bob@example.com',
+            'username': 'alice',
+            'email': 'alice@example.com',
             'password': 'secret',
         },
     )
 
     response_error = client.put(
-        'users/-1',
+        '/users/-1',
         json={
-            'username': 'bob',
-            'email': 'bob@example.com',
+            'username': 'alice',
+            'email': 'alice@example.com',
             'password': 'secret',
         },
     )
@@ -78,24 +80,43 @@ def test_update_user(client):
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {
         'id': 1,
-        'username': 'bob',
-        'email': 'bob@example.com',
+        'username': 'alice',
+        'email': 'alice@example.com',
     }
 
     assert response_error.status_code == HTTPStatus.NOT_FOUND
     assert response_error.json() == {'detail': 'User not found!'}
 
 
-def test_delete_user(client):
+def test_update_integrity_error(client, user):
+    client.post(
+        '/users',
+        json={
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'secret',
+        },
+    )
+
+    response = client.put(
+        f'/users/{user.id}',
+        json={
+            'username': 'bob',
+            'email': 'regis@example.com',
+            'password': 'new_secret',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CONFLICT
+    assert response.json() == {'detail': 'Username or Email already exists'}
+
+
+def test_delete_user(client, user):
     response = client.delete('/users/1')
     response_error = client.delete('users/-1')
 
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {
-        'id': 1,
-        'username': 'bob',
-        'email': 'bob@example.com',
-    }
+    assert response.json() == {'message': 'User delete'}
 
     assert response_error.status_code == HTTPStatus.NOT_FOUND
     assert response_error.json() == {'detail': 'User not found!'}
